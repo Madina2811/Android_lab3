18191e909e66429d5b3bb6c07bdbcb0589ba9dab
Madina2811 <gasanowamadina@yandex.ru>
2020-11-11 08:21:01 +0400

    lab3-fake.apk

--- lab3-fake/app/src/main/java/sg/vantagepoint/a/b.java
+++
@@ -1,9 +0,0 @@
-package sg.vantagepoint.a;
-
-import android.content.Context;
-
-public class b {
-    public static boolean a(Context context) {
-        return (context.getApplicationContext().getApplicationInfo().flags & 2) != 0;
-    }
-}
